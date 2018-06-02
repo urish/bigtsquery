052de21d1dae7b9127ef5b6c5449b7ee70cd4288
Uri Shaked <uri@urishaked.com>
2018-06-02 20:47:25 +0300

    refactor: move presets from template to array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AstSearchService, IQueryResult } from './ast-search.service';
 
+interface IPreset {
+  name: string;
+  value: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +16,17 @@ export class AppComponent {
   public astQuery = 'FunctionDeclaration:has(ExportKeyword)>Identifier';
   public searching = false;
   public results: IQueryResult[];
+
+  readonly presets: IPreset[] = [
+    { name: 'Exported Functions', value: 'FunctionDeclaration:has(ExportKeyword)>Identifier' },
+    { name: 'Exported Classes', value: 'ClassDeclaration:has(ExportKeyword)>Identifier' },
+    { name: 'Angular Components', value: 'Decorator>CallExpression[expression.name=Component]' },
+    { name: 'Vue Components', value: 'ClassDeclaration:has(HeritageClause Identifier[name=Vue])' },
+    {
+      name: 'BDD Tests',
+      value: 'CallExpression[expression.name=describe] CallExpression[expression.name=it]',
+    },
+  ];
 
   constructor(private astSearch: AstSearchService) {}
 
