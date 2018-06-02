3172659e311da30af9e7d7fc624cb7d43f7d7a4b
Uri Shaked <uri@urishaked.com>
2018-06-02 21:25:46 +0300

    feat(functions): cache query results to Cloud Firestore
    close #3

--- functions/src/ast-query.ts
+++ functions/src/ast-query.ts
@@ -1,7 +1,9 @@
-import * as e from 'express';
 import * as Bigquery from '@google-cloud/bigquery';
-import { getSqlQuery } from './sql-query';
+import * as e from 'express';
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 import { IMatch } from './search-utils';
+import { getSqlQuery } from './sql-query';
 
 const credentials = require('../bigquery-credentials.json');
 
@@ -10,23 +12,52 @@ const bigquery = Bigquery({
   projectId: credentials.project_id,
 });
 
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
+const queriesCollection = firestore.collection('queries');
+
 interface IQueryResult {
   id: string;
   paths: string[];
   match: string;
 }
 
+interface ICacheEntry {
+  query: string;
+  time: Date;
+  results: IQueryResult[];
+}
+
+async function executeQuery(query: string) {
+  const [result] = await bigquery.query<IQueryResult>({
+    query: getSqlQuery(),
+    params: [query],
+    maxResults: 10,
+  });
+  return result;
+}
+
 export async function astQuery(request: e.Request, response: e.Response) {
   const { q } = request.query;
+  const query = q.trim();
   console.log(`[${request.ip}] Query: ${q}`);
   try {
-    const [result] = await bigquery.query<IQueryResult>({
-      query: getSqlQuery(),
-      params: [q],
-      maxResults: 10,
-    });
+    const snapshot = await queriesCollection.where('query', '==', query).get();
+    let results: IQueryResult[];
+    if (snapshot.docs.length) {
+      const cacheEntry = snapshot.docs[0].data() as ICacheEntry;
+      results = cacheEntry.results;
+    } else {
+      const startTime = new Date().getTime();
+      results = await executeQuery(query);
+      await queriesCollection.add({
+        query,
+        results,
+        time: new Date(),
+      } as ICacheEntry);
+    }
     response.json(
-      result.map((entry) => ({
+      results.map((entry) => ({
         id: entry.id,
         paths: entry.paths,
         ...(JSON.parse(entry.match) as IMatch),
